7bb1c698b8f9904e4a142bfbe6120bcf9b322740
OutdatedGuy <everythingoutdated@gmail.com>
2025-03-21 03:31:29 +0530

    fix: method channel not detached from engine with flutter

--- android/src/main/kotlin/com/razorpay/razorpay_flutter/RazorpayFlutterPlugin.kt
+++ android/src/main/kotlin/com/razorpay/razorpay_flutter/RazorpayFlutterPlugin.kt
@@ -11,15 +11,23 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 class RazorpayFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
+
     private var razorpayDelegate: RazorpayDelegate? = null
     private var pluginBinding: ActivityPluginBinding? = null
 
-    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        val channel = MethodChannel(binding.binaryMessenger, "razorpay_flutter")
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "razorpay_flutter")
         channel.setMethodCallHandler(this)
     }
 
-    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
         when (call.method) {
